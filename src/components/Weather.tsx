cafc58352480d826c00a81a10f1fa5b13c269865
Plamena Baksheva <p.baksheva@blubito.com>
2022-02-08 19:03:19 +0200

    added comment

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -5,6 +5,7 @@ export default function Weather() {
     const [city, setCity] = useState("");
     const [sofiaWeather, setSofiaWeather] = useState("Weather will be shown here:");
     //your code goes here = useEffect + AXIOS/FETCH
+    //git changes
     useEffect(() => {
         const fetchData = async () => {
             const { data } = await axios('https://meta-weather.vercel.app/api/location/839722/');
